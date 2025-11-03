0ec22e305a97f62767ab5e3da7f2fe16df288131
dimamaksymiuk01 <dimamaksymiuk01@gmail.com>
2025-11-03 04:10:10 +0200

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,35 @@
+import fs from 'fs';
 import path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    {
+      name: 'create-404-redirect',
+      closeBundle() {
+        const outDir = path.resolve(__dirname, 'docs');
+        const file = path.join(outDir, '404.html');
+
+        if (!fs.existsSync(outDir)) {
+          fs.mkdirSync(outDir, { recursive: true });
+        }
+
+        fs.writeFileSync(
+          file,
+          `<meta http-equiv="refresh" content="0; url=/react-todo-list/" />`,
+        );
+      },
+    },
+  ],
+
   base: '/react-todo-list/',
   build: {
     outDir: 'docs',
+    emptyOutDir: true,
   },
   resolve: {
     alias: {
