ed298ce4d6654f614398051f9cc285d69b4f61c8
dimamaksymiuk01 <dimamaksymiuk01@gmail.com>
2025-11-05 12:32:51 +0200

    deploy fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,15 @@ import { AppRouter } from '@/router/AppRouter';
 import '@/i18n/config';
 import './assets/global.css';
 
+const redirectPath = sessionStorage.getItem('redirectPath');
+if (redirectPath) {
+  sessionStorage.removeItem('redirectPath');
+  const cleanPath = redirectPath.replace('/react-todo-list', '');
+  if (cleanPath && cleanPath !== '/') {
+    window.history.replaceState(null, '', '/react-todo-list' + cleanPath);
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename='/react-todo-list/'>
     <ThemeProvider>
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,14 +1,11 @@
 import { FC } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-import { LanguageRedirect } from './LanguageRedirect';
-
 import App from '@/App';
 
 export const AppRouter: FC = () => {
   return (
     <>
-      <LanguageRedirect />
       <Routes>
         <Route path='/' element={<App />} />
         <Route path='/ua/*' element={<App />} />
